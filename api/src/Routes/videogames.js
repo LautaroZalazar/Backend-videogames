979047641562b8073f3cf414cb680666ec6244e8
LautaroZalazar <lautyzalazar10@gmail.com>
2022-07-31 22:59:24 -0300

    Ruta obtener detalle de videojuego

--- api/src/Routes/videogames.js
+++ api/src/Routes/videogames.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import getAll  from "../Controllers/Videogames/GET/getall.js"
+import getVGById from "../Controllers/Videogames/GET/getVGById.js";
 import PostVG from "../Controllers/Videogames/POST/postVG.js";
 import PostVGAPI from "../Controllers/Videogames/POST/postVGAPI.js";
 const router = Router();
 
 router.get("/", getAll)
+
+router.get("/:id", getVGById)
 
 router.post("/", PostVG)
 
